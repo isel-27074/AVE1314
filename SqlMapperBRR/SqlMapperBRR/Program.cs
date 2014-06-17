fd078ed3a05721cf32ec1a529f121cdef411f851
Ricardo Marta <a27074@alunos.isel.pt>
2014-06-17 19:57:45 +0100

    escolha de data source

--- SqlMapperBRR/SqlMapperBRR/Program.cs
+++ SqlMapperBRR/SqlMapperBRR/Program.cs
@@ -13,13 +13,43 @@ namespace SqlMapperBRR
         static void Main(string[] args)
         {
             SqlConnection con = new SqlConnection();
+
+            string datasource = null;
+
+            while (datasource == null || datasource.Equals(""))
+            {
+                Console.WriteLine("Escolha Data Source:");
+                Console.WriteLine("A - PCK8");
+                Console.WriteLine("B - Darkstar\\Sqlexpress");
+                Console.WriteLine("C - Bruno?? ");
+                Console.WriteLine("D - Introduzir nova DS");
+                datasource = Console.ReadLine();
+                switch (datasource.ToUpper())
+                {
+                    case "A":
+                        datasource = "PCK8";
+                        break;
+                    case "B":
+                        datasource = "darkstar\\sqlexpress";
+                        break;
+                    case "C":
+                        datasource = "";
+                        break;
+                    default:
+                        Console.WriteLine("Inserir Data Source:");
+                        datasource = Console.ReadLine();
+                        break;
+
+                }
+            }
+
+            Console.WriteLine(datasource);
             try
             {
-                con.ConnectionString = @"Data Source=DARKSTAR\SQLEXPRESS; 
-                                        Initial Catalog=ave; 
-                                        Integrated Security=True";
+                con.ConnectionString = @"Data Source=" + datasource + "; Initial Catalog=ave; Integrated Security=True";
                 SqlCommand cmd = con.CreateCommand();
                 cmd.CommandText = "SELECT * from Product";
+                Console.WriteLine("Openning connection...");
                 con.Open();
                 SqlDataReader dr = cmd.ExecuteReader();
                 //dr.GetDataTypeName
@@ -28,7 +58,7 @@ namespace SqlMapperBRR
                 //    //Console.WriteLine(dr["productName"]);
                 //    Console.WriteLine(o.ToString());
                 //}
-                int count=0;
+                int count = 0;
 
 
                 while (dr.Read())
@@ -36,10 +66,15 @@ namespace SqlMapperBRR
                     Console.WriteLine(dr[count]);
                     Console.WriteLine(dr["productName"]);
                 }
-
             }
+            catch (SqlException se) {
+                Console.WriteLine(se.Message);
+            }
+
             finally
             {
+                Console.WriteLine("Ending connection...");
+                Console.ReadKey(); 
                 if (con.State != ConnectionState.Closed)
                     con.Dispose();
                     //con.Close();
