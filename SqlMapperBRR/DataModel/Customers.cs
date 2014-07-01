9a9a4696f8adc7d08b2a7b24f1e385e016e24f9d
BrunoFontainha258 <brunofontainha258@gmail.com>
2014-07-01 18:09:58 +0100

    Data Model With Constructers

--- SqlMapperBRR/DataModel/Customers.cs
+++ SqlMapperBRR/DataModel/Customers.cs
@@ -22,5 +22,30 @@ namespace DataModel
         public string PostalCode { set; get; }
         public string Country { set; get; }
         public int Phone { set; get; }
+
+        public Customers() { }
+
+        public Customers(string CompanyName, string ContactName, string ContactTitle, string Address, string City, string Region, string PostalCode, string Country, string Phone)
+            : this(0, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone)
+        {
+        }
+
+        public Customers(int CustomerID, string CompanyName, string ContactName, string ContactTitle, string Address, string City, string Region, string PostalCode, string Country, string Phone)
+        {
+            this.CustomerID = CustomerID;
+            this.CompanyName = CompanyName;
+            this.ContactName = ContactTitle;
+            this.Address = Address;
+            this.City = City;
+            this.Region = Region;
+            this.PostalCode = PostalCode;
+            this.Country = Country;
+            this.Phone = Phone;
+        }
+
+        public override string ToString()
+        {
+            return CustomerID + " - " + CompanyName + " - " + ContactName + " - " + Address + " - " + City + " - " + Region + "-" + PostalCode + "-" + Country + "-" + Phone;
+        }
     }
 }
--- SqlMapperBRR/DataModel/Orders.cs
+++ SqlMapperBRR/DataModel/Orders.cs
@@ -8,7 +8,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataModel
 {
-    [Table("Employees")]
+    [Table("Orders")]
     class Orders
     {
         [Key]
@@ -29,6 +29,36 @@ namespace DataModel
         public string ShipRegion { set; get; }
         public string ShipPostalCode { set; get; }
         public string ShipCountry { set; get; }
+
+        public Orders() { }
+
+        public Orders(int CustomerID, int EmployeeID, string OrderDAte, string RequiredDate, string ShippedDate, string ShipVia, string Freight, string ShipName, string ShipAddress, string ShipCity, string ShipRegion, string ShipPostalCode, string ShipCountry)
+            : this(0,CustomerID, EmployeeID, OrderDAte, RequiredDate, ShippedDate, ShipVia,Freight,ShipName,ShipAddress,ShipCity,ShipRegion,ShipPostalCode,ShipCountry)
+        {
+        }
+
+        public Orders(int OrderID, int CustomerID, int EmployeeID, string OrderDAte, string RequiredDate, string ShippedDate, string ShipVia, string Freight, string ShipName, string ShipAddress, string ShipCity, string ShipRegion, string ShipPostalCode, string ShipCountry)
+        {
+			this.OrderID = OrderID;
+			this.CustomerID = CustomerID;
+			this.EmployeeID = EmployeeID;
+			this.OrderDAte = OrderDAte;
+			this.RequiredDate = RequiredDate;
+			this.ShippedDate = ShippedDate;
+			this.ShipVia = ShipVia;
+			this.Freight = Freight;
+			this.ShipName = ShipName;
+			this.ShipAddress = ShipAddress;
+			this.ShipCity = ShipCity;
+			this.ShipRegion = ShipRegion;
+			this.ShipPostalCode = ShipPostalCode; 
+			this.ShipCountry = ShipCountry;
+        }
+
+        public override string ToString()
+        {
+            return OrderID + " - " + CustomerID + " - " + EmployeeID + " - " + OrderDAte + " - " + RequiredDate + " - " + ShippedDate + " - " + ShipVia + " - "+ Freight + " - " + ShipName + " - " + ShipAddress + " - " + ShippedDate + " - " + ShipCity + " - " + ShipRegion + "-" + ShipPostalCode + "-" + ShipCountry;
+        }
 
 
 
