b591ffe1742f2f3390b47b56d3a62576c359c47e
ruieskilo <rui_eskilo@hotmail.com>
2014-07-03 20:36:57 +0100

    a compilar...

--- SqlMapperBRR/SqlMapper_v2/SQLEnumerable.cs
+++ SqlMapperBRR/SqlMapper_v2/SQLEnumerable.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,35 +22,61 @@ namespace SqlMapper_v2
         {
             _command.CommandText = (!_command.CommandText.Contains("where") ? "+ where" : "");
             _command.CommandText += "and" + clause.ToString();
-            return new SQLEnumerable<T>(_command.CommandText);
+            //return new SQLEnumerable<T>(_command.CommandText);
+            return this;
 
-           /* if (!_persistant) _connnection.Open();
-            if (_connnection.State != ConnectionState.Open)
-                _connnection.Open(); //abre se não estava aberta
-            PreparedGetAll(FormatStringGetAll(_table));
-            _dr = _command.ExecuteReader();
+            /* if (!_persistant) _connnection.Open();
+             if (_connnection.State != ConnectionState.Open)
+                 _connnection.Open(); //abre se não estava aberta
+             PreparedGetAll(FormatStringGetAll(_table));
+             _dr = _command.ExecuteReader();
 
-            //int numberOfColumns = 0; //to remove
-            foreach (var dr in _dr)
-            {
-                //Console.WriteLine(_dr.GetFieldType(numberOfColumns).Name); //to remove
-                object[] o = new object[_columns.Length];
-                for (int i = 0; i < _columns.Length; i++)
-                {
-                    //Console.WriteLine(_dr[i]); //to remove
-                    o[i] = _dr[i];
-                }
-                T newT = (T)Activator.CreateInstance(typeof(T), o);
-                yield return newT;
-            }
+             //int numberOfColumns = 0; //to remove
+             foreach (var dr in _dr)
+             {
+                 //Console.WriteLine(_dr.GetFieldType(numberOfColumns).Name); //to remove
+                 object[] o = new object[_columns.Length];
+                 for (int i = 0; i < _columns.Length; i++)
+                 {
+                     //Console.WriteLine(_dr[i]); //to remove
+                     o[i] = _dr[i];
+                 }
+                 T newT = (T)Activator.CreateInstance(typeof(T), o);
+                 yield return newT;
+             }
 
-            if (!_persistant) _connnection.Close();
-            _dr.Close();
-        }*/
+             if (!_persistant) _connnection.Close();
+             _dr.Close();
+         }*/
+
+        }
+        //getall
+        //public IEnumerator<T> GetEnumerator()
+        //{
+        //    return null;
+        //}
 
 
 
-       
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    return GetEnumerator();
+        //}
 
+
+        //public ISqlEnumerable<T> @where(string clause, SqlConnection con, bool persistant, string table, string[] columns)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
