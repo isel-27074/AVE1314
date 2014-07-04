326519733100b9c107b141ca36341e7de9f482bd
Ricardo Marta <a27074@alunos.isel.pt>
2014-07-04 21:25:38 +0100

    correcção de Customer

--- SQLAVE-PopulateData.sql
+++ SQLAVE-PopulateData.sql
@@ -13,14 +13,14 @@ INSERT INTO Products VALUES ('Abóbora', '1', 4, 5, 2);
 
 /*
 [Table] Customers
-[String] IDENTITY(1,1)
+[String]
 [CustomerID][CompanyName][ContactName][ContactTitle][Address][City][Region][PostalCode][Country][Phone][Fax]*/
-INSERT INTO Customers VALUES ('1','Company1', 'Contact1', 'Mr' ,'Rua xxx' ,'Porto' ,' North' ,'4950 ' ,' Portugal' ,' 91123456' ,'2151421 ' );
-INSERT INTO Customers VALUES ('2','Company2', 'Contact2', 'Mrs' ,'Rua yy' ,'Porto' ,' North' ,'4950 ' ,' Portugal' ,' 91123456' ,'2151421 ' );
-INSERT INTO Customers VALUES ('3','Company3', 'Contact3', 'Ms' ,'Rua qq' ,'Porto' ,' North' ,'4950 ' ,' Portugal' ,' 91123456' ,'2151421 ' );
-INSERT INTO Customers VALUES ('4','Company4', 'Contact4', 'Mr' ,'Rua www' ,'Lisbon' ,' Center' ,'4950 ' ,' Portugal' ,' 91123456' ,'2151421 ' );
-INSERT INTO Customers VALUES ('5','Company5', 'Contact5', 'Mrs' ,'Rua eee' ,'Sevilha' ,' Center' ,'4950 ' ,' Spain' ,' 91123456' ,'2151421 ' );
-INSERT INTO Customers VALUES ('6','Company6', 'Contact6', 'Ms' ,'Rua rrrr' ,'Sevilha' ,' Center' ,'4950 ' ,' Spain' ,' 91123456' ,'2151421 ' );
+INSERT INTO Customers VALUES ('C0001','Company1', 'Contact1', 'Mr' ,'Rua xxx' ,'Porto' ,' North' ,'4950 ' ,' Portugal' ,' 91123456' ,'2151421 ' );
+INSERT INTO Customers VALUES ('C0002','Company2', 'Contact2', 'Mrs' ,'Rua yy' ,'Porto' ,' North' ,'4950 ' ,' Portugal' ,' 91123456' ,'2151421 ' );
+INSERT INTO Customers VALUES ('C0003','Company3', 'Contact3', 'Ms' ,'Rua qq' ,'Porto' ,' North' ,'4950 ' ,' Portugal' ,' 91123456' ,'2151421 ' );
+INSERT INTO Customers VALUES ('C0004','Company4', 'Contact4', 'Mr' ,'Rua www' ,'Lisbon' ,' Center' ,'4950 ' ,' Portugal' ,' 91123456' ,'2151421 ' );
+INSERT INTO Customers VALUES ('C0005','Company5', 'Contact5', 'Mrs' ,'Rua eee' ,'Sevilha' ,' Center' ,'4950 ' ,' Spain' ,' 91123456' ,'2151421 ' );
+INSERT INTO Customers VALUES ('C0006','Company6', 'Contact6', 'Ms' ,'Rua rrrr' ,'Sevilha' ,' Center' ,'4950 ' ,' Spain' ,' 91123456' ,'2151421 ' );
 
 /*
 [Table] Employees
@@ -35,8 +35,8 @@ INSERT INTO Employees VALUES ('Domingos', 'Diana', 'Ms', 'Ms', '1900-01-01 12:10
 [Table] Orders
 [int] IDENTITY(1,1)
 [OrderID][CustomerID][EmployeeID][OrderDate][RequiredDate][ShippedDate][ShipVia][Freight][ShipName][ShipAddress][ShipCity][ShipRegion][ShipPostalCode][ShipCountry]*/
-INSERT INTO Orders VALUES (1,1,'1900-01-01 12:10:05.123','1900-01-01 12:10:05.123','1900-01-01 12:10:05.123',2,22,'Narvas','Rua yyy', 'Porto', 'North', '1234', 'Portugal');
-INSERT INTO Orders VALUES (2,2,'1900-01-01 12:10:05.123','1900-01-01 12:10:05.123','1900-01-01 12:10:05.123',1,33,'Chronopost','Rua xxx', 'Braga', 'North', '1234', 'Portugal');
-INSERT INTO Orders VALUES (3,3,'1900-01-01 12:10:05.123','1900-01-01 12:10:05.123','1900-01-01 12:10:05.123',0,44,'UPS','Rua qqq', 'Lisbon', 'Center', '1234', 'Portugal');
-INSERT INTO Orders VALUES (4,4,'1900-01-01 12:10:05.123','1900-01-01 12:10:05.123','1900-01-01 12:10:05.123',3,55,'Chronopost','Rua www', 'Faro', 'South', '1234', 'Portugal');
-INSERT INTO Orders VALUES (5,5,'1900-01-01 12:10:05.123','1900-01-01 12:10:05.123','1900-01-01 12:10:05.123',4,66,'UPS','Rua eee', 'Beja', 'South', '1234', 'Portugal');
+INSERT INTO Orders VALUES ('C0001',1,'1900-01-01 12:10:05.123','1900-01-01 12:10:05.123','1900-01-01 12:10:05.123',2,22,'Narvas','Rua yyy', 'Porto', 'North', '1234', 'Portugal');
+INSERT INTO Orders VALUES ('C0001',2,'1900-01-01 12:10:05.123','1900-01-01 12:10:05.123','1900-01-01 12:10:05.123',1,33,'Chronopost','Rua xxx', 'Braga', 'North', '1234', 'Portugal');
+INSERT INTO Orders VALUES ('C0003',3,'1900-01-01 12:10:05.123','1900-01-01 12:10:05.123','1900-01-01 12:10:05.123',0,44,'UPS','Rua qqq', 'Lisbon', 'Center', '1234', 'Portugal');
+INSERT INTO Orders VALUES ('C0004',4,'1900-01-01 12:10:05.123','1900-01-01 12:10:05.123','1900-01-01 12:10:05.123',3,55,'Chronopost','Rua www', 'Faro', 'South', '1234', 'Portugal');
+INSERT INTO Orders VALUES ('C0004',5,'1900-01-01 12:10:05.123','1900-01-01 12:10:05.123','1900-01-01 12:10:05.123',4,66,'UPS','Rua eee', 'Beja', 'South', '1234', 'Portugal');
